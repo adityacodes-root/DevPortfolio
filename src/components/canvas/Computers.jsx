6a1fc2cd6a2ead21a093e06e1f3c416e956b62a3
Aditya <aditya.d2676@gmail.com>
2025-02-27 00:08:52 +0530

    .

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -9,21 +9,21 @@ const Computers = ({ isMobile }) => {
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor='red' />
+      <hemisphereLight intensity={0.2} groundColor="red" />
       <spotLight
-        position={[-20, 50, 5]}
-        angle={0.12}
+        position={[-15, 40, 10]}
+        angle={0.2}
         penumbra={1}
-        intensity={1}
+        intensity={1.2}
         castShadow
         shadow-mapSize={1024}
       />
-      <pointLight intensity={0.3} />
+      <pointLight intensity={0.5} />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 7.7 : 12.75}
-        position={isMobile ? [-0.6, -0.5, -2.5] : [-1.9, -0.5, -4.5]}
-        rotation={[-0.01, 0.4, -0.0]}
+        scale={isMobile ? 0.5 : 0.65}
+        position={isMobile ? [0, 1.2, -2.3] : [0, -1, -3.5]}
+        rotation={[-0.05, 0.3, -0.02]}
       />
     </mesh>
   );
@@ -33,21 +33,14 @@ const ComputersCanvas = () => {
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
-    // Add a listener for changes to the screen size
     const mediaQuery = window.matchMedia("(max-width: 500px)");
-
-    // Set the initial value of the `isMobile` state variable
     setIsMobile(mediaQuery.matches);
 
-    // Define a callback function to handle changes to the media query
     const handleMediaQueryChange = (event) => {
       setIsMobile(event.matches);
     };
 
-    // Add the callback function as a listener for changes to the media query
     mediaQuery.addEventListener("change", handleMediaQueryChange);
-
-    // Remove the listener when the component is unmounted
     return () => {
       mediaQuery.removeEventListener("change", handleMediaQueryChange);
     };
@@ -55,17 +48,17 @@ const ComputersCanvas = () => {
 
   return (
     <Canvas
-      frameloop='demand'
+      frameloop="demand"
       shadows
       dpr={[1, 2]}
-      camera={{ position: [20, 10, 10], fov: 28 }}
+      camera={{ position: [8, 3, 10], fov: 30 }}
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
           enableZoom={false}
-          maxPolarAngle={Math.PI / 2.8}
-          minPolarAngle={Math.PI / 2.8}
+          maxPolarAngle={Math.PI / 2.5}
+          minPolarAngle={Math.PI / 2.5}
           autoRotate={true}
         />
         <Computers isMobile={isMobile} />
